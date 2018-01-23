51f0661e572b91c0519e6ec9f8be1d5b4db0d630
Jarvis\Suresh <smsureshb4u@gmail.com>
2018-01-24 00:21:55 +0530

    url changed

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -5,14 +5,23 @@ import {ApiConfigService} from './api-config.service';
 import { HttpClient, HttpHeaders,HttpHandler } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, x-auth"
+ })
+};
+
 @Injectable()
 export class LoginService {
+  
 
   constructor(private apiConfig:ApiConfigService,private http: HttpClient) { }
 
 authenticate(data: Object): Observable<any> {
-  var url = this.apiConfig.hostUrl+this.apiConfig.baseUrl+"login";
-  return this.http.post<any>(url, data).pipe(
+  var url = this.apiConfig.hostUrl+"login";
+  return this.http.post<any>(url, data, httpOptions).pipe(
     tap((res) => console.log(res))
   );
 }
